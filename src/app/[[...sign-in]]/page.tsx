dee69b264d59f8bcdf0017fa0e8f5e1c2bd217e9
farzadMB03 <clashfarzad82@gmail.com>
2025-01-28 20:47:41 +0330

    git commit -m

---
+++ src/app/[[...sign-in]]/page.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+import * as Clerk from "@clerk/elements/common";
+import * as SignIn from "@clerk/elements/sign-in";
+import { useUser } from "@clerk/nextjs";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+const LoginPage = () => {
+  const { isLoaded, isSignedIn, user } = useUser();
+  console.log(user);
+  const router = useRouter();
+
+  useEffect(() => {
+    const role = user?.publicMetadata.role;
+    console.log(role);
+
+    if (role) {
+      router.push(`/${role}`);
+    }
+  }, [user, router]);
+
+  return (
+    <div className="bg-customPink h-screen flex items-center justify-center bg-SchoolSkyLight">
+      <SignIn.Root>
+        <SignIn.Step
+          name="start"
+          className="bg-customDark p-12 rounded-md shadow-2xl flex flex-col gap-2"
+        >
+          <h1 className="text-customPurple text-xl font-bold flex items-center gap-2">
+            <Image src="/logo.png" alt="" width={24} height={24} />
+            School
+          </h1>
+          <h2 className="text-white">Sign in to your account</h2>
+          <Clerk.GlobalError className="text-sm text-red-400" />
+          <Clerk.Field name="identifier" className="flex flex-col gap-2">
+            <Clerk.Label className="text-xs text-gray-400">
+              Username
+            </Clerk.Label>
+            <Clerk.Input
+              type="text"
+              required
+              className="p-2 rounded-md ring-1 ring-gray-300"
+            />
+            <Clerk.FieldError className="text-xs text-red-400" />
+          </Clerk.Field>
+          <Clerk.Field name="password" className="flex flex-col gap-2">
+            <Clerk.Label className="text-xs text-gray-400">
+              Password
+            </Clerk.Label>
+            <Clerk.Input
+              type="password"
+              required
+              className="p-2 rounded-md ring-1 ring-gray-300"
+            />
+            <Clerk.FieldError className="text-xs text-red-400" />
+          </Clerk.Field>
+          <SignIn.Action
+            submit
+            className="bg-customPurple text-white my-1 rounded-md text-sm p-[10px]"
+          >
+            Sign In
+          </SignIn.Action>
+        </SignIn.Step>
+      </SignIn.Root>
+    </div>
+  );
+};
+
+export default LoginPage;
---
+++ src/components/forms/EventForm.tsx
@@ -0,0 +1,126 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import InputField from "../InputField";
+import {
+  eventSchema,
+  EventSchema,
+  StudentSchema,
+} from "@/lib/formValidationSchemas";
+import {
+  createEvent,
+  createSubject,
+  updateEvent,
+  updateSubject,
+} from "@/lib/actions";
+import { useFormState } from "react-dom";
+import { Dispatch, SetStateAction, useEffect } from "react";
+import { toast } from "react-toastify";
+import { useRouter } from "next/navigation";
+
+const EventForm = ({
+  type,
+  data,
+  setOpen,
+  relatedData,
+}: {
+  type: "create" | "update";
+  data?: any;
+  setOpen: Dispatch<SetStateAction<boolean>>;
+  relatedData?: any;
+}) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<EventSchema>({
+    resolver: zodResolver(eventSchema),
+  });
+
+  // AFTER REACT 19 IT'LL BE USEACTIONSTATE
+
+  const [state, formAction] = useFormState(
+    type === "create" ? createEvent : updateEvent,
+    {
+      success: false,
+      error: false,
+    }
+  );
+
+  const onSubmit = handleSubmit((data) => {
+    console.log(data);
+    formAction(data);
+  });
+
+  const router = useRouter();
+
+  useEffect(() => {
+    if (state.success) {
+      toast(`Subject has been ${type === "create" ? "created" : "updated"}!`);
+      setOpen(false);
+      router.refresh();
+    }
+  }, [state, router, type, setOpen]);
+
+  const { teachers } = relatedData;
+
+  return (
+    <form className="flex flex-col gap-8" onSubmit={onSubmit}>
+      <h1 className="text-xl font-semibold text-white">
+        {type === "create" ? "Create a new event" : "Update the event"}
+      </h1>
+
+      <div className="flex flex-wrap gap-4">
+        <InputField
+          label="Title"
+          name="title"
+          defaultValue={data?.title}
+          register={register}
+          error={errors.title}
+        />
+        <InputField
+          label="Description"
+          name="description"
+          defaultValue={data?.description}
+          register={register}
+          error={errors.description}
+        />
+        <div className="w-full md:w-1/2">
+          <label className="text-xs text-gray-500">Start Time</label>
+          <input
+            type="datetime-local"
+            className="ring-[1.5px] ring-gray-300 p-2 rounded-md text-sm w-full"
+            {...register("startTime")}
+            defaultValue={data?.startTime}
+          />
+          {errors.startTime?.message && (
+            <p className="text-xs text-red-400">
+              {errors.startTime.message.toString()}
+            </p>
+          )}
+        </div>
+        <div className="w-full md:w-1/2">
+          <label className="text-xs text-gray-500">End Time</label>
+          <input
+            type="datetime-local"
+            className="ring-[1.5px] ring-gray-300 p-2 rounded-md text-sm w-full"
+            {...register("endTime")}
+            defaultValue={data?.endTime}
+          />
+          {errors.endTime?.message && (
+            <p className="text-xs text-red-400">
+              {errors.endTime.message.toString()}
+            </p>
+          )}
+        </div>
+      </div>
+
+      <button className="bg-customPurple text-white p-2 rounded-md">
+        {type === "create" ? "Create" : "Update"}
+      </button>
+    </form>
+  );
+};
+
+export default EventForm;
