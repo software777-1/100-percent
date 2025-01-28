4c0f56cc88fe58af1e541ffc7f29afe338fce8a7
AminMokhtarvand <amin.mokhtarvand1@gmail.com>
2025-01-29 00:02:24 +0330

    git config -m " cartesiangrid problem"

--- src/components/FinanceChart.tsx
+++ src/components/FinanceChart.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Image from "next/image";
 import {
   LineChart,
   Line,
@@ -12,87 +11,32 @@ import {
   ResponsiveContainer,
 } from "recharts";
 
-const data = [
-  {
-    name: "Jan",
-    income: 4000,
-    expense: 2400,
-  },
-  {
-    name: "Feb",
-    income: 3000,
-    expense: 1398,
-  },
-  {
-    name: "Mar",
-    income: 2000,
-    expense: 9800,
-  },
-  {
-    name: "Apr",
-    income: 2780,
-    expense: 3908,
-  },
-  {
-    name: "May",
-    income: 1890,
-    expense: 4800,
-  },
-  {
-    name: "Jun",
-    income: 2390,
-    expense: 3800,
-  },
-  {
-    name: "Jul",
-    income: 3490,
-    expense: 4300,
-  },
-  {
-    name: "Aug",
-    income: 3490,
-    expense: 4300,
-  },
-  {
-    name: "Sep",
-    income: 3490,
-    expense: 4300,
-  },
-  {
-    name: "Oct",
-    income: 3490,
-    expense: 4300,
-  },
-  {
-    name: "Nov",
-    income: 3490,
-    expense: 4300,
-  },
-  {
-    name: "Dec",
-    income: 3490,
-    expense: 4300,
-  },
-];
-
 const FinanceChart = () => {
+  const data = [
+    { name: "Jan", income: 4000, expense: 2400 },
+    { name: "Feb", income: 3000, expense: 1398 },
+    { name: "Mar", income: 2000, expense: 9800 },
+    { name: "Apr", income: 2780, expense: 3908 },
+    { name: "May", income: 1890, expense: 4800 },
+    { name: "Jun", income: 2390, expense: 3800 },
+    { name: "Jul", income: 3490, expense: 4300 },
+    { name: "Aug", income: 3490, expense: 4300 },
+    { name: "Sep", income: 3490, expense: 4300 },
+    { name: "Oct", income: 3490, expense: 4300 },
+    { name: "Nov", income: 3490, expense: 4300 },
+    { name: "Dec", income: 3490, expense: 4300 },
+  ];
+
   return (
     <div className="rounded-xl w-full h-full p-4">
-      <div className="flex justify-between items-center">
+      <div className="mb-4 flex justify-between items-center">
         <h1 className="text-lg font-semibold text-customWhite">Finance</h1>
-        <Image src="/moreDark.png" alt="" width={20} height={20} />
+        <img src="/moreDark.png" alt="More" width={20} height={20} />
       </div>
       <ResponsiveContainer width="100%" height="90%">
         <LineChart
-          width={500}
-          height={300}
           data={data}
-          margin={{
-            top: 5,
-            right: 30,
-            left: 20,
-            bottom: 5,
-          }}
+          margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
         >
           <CartesianGrid strokeDasharray="3 3" stroke="#ddd" />
           <XAxis
@@ -108,23 +52,25 @@ const FinanceChart = () => {
             tickLine={false}
             tickMargin={20}
           />
-          <Tooltip />
+          <Tooltip
+            contentStyle={{ borderRadius: "8px", borderColor: "#ddd" }}
+          />
           <Legend
             align="center"
             verticalAlign="top"
-            wrapperStyle={{ paddingTop: "10px", paddingBottom: "30px" }}
+            wrapperStyle={{ paddingTop: "10px" }}
           />
           <Line
             type="monotone"
             dataKey="income"
             stroke="#A7FFEB"
-            strokeWidth={5}
+            strokeWidth={4}
           />
           <Line
             type="monotone"
             dataKey="expense"
             stroke="#E0BBE4"
-            strokeWidth={5}
+            strokeWidth={4}
           />
         </LineChart>
       </ResponsiveContainer>
