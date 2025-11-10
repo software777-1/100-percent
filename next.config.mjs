003b4d326b3ea249aa0abb55d37198027a6547b7
Soheil <soheilalimohammadi@yahoo.com>
2025-11-10 16:55:25 +0330

    modifications and final touches

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
+/** this is a Next.js configuration file, it was ensembled by G_Group */
 const nextConfig = {
   images: {
     remotePatterns: [{ hostname: "res.cloudinary.com" }],
+  
   },
 };
 
--- postcss.config.mjs
+++ postcss.config.mjs
@@ -4,5 +4,6 @@ const config = {
     tailwindcss: {},
   },
 };
-
+/**this is for postcss */
+/** ds*/
 export default config;
